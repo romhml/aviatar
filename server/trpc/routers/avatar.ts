fe6bda7c3f0a5b228571d98a45a9947c499d181c
Romain Hamel <romain@boilr.io>
2023-10-13 14:05:54 +0200

    feat: improve inpainting model and prepare for multiple lora models

--- server/trpc/routers/avatar.ts
+++ server/trpc/routers/avatar.ts
@@ -6,17 +6,32 @@ const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
 })
 
+type Model = {
+  version: string,
+}
+
+const models: Record<string, Model>= {
+  inpaint: {
+    version: 'c11bac58203367db93a3c552bd49a25a5418458ddffb7e90dae55780765e26d6',
+  },
+  vangogh: {
+    version: '2d43b996608bd7d4aba4cacbe9b751399892a9d6cbc27a39f8f49347a3a16f9c',
+  }
+}
+
 const generateInput = z.object({
   prompt: string(),
   avatar: string().nullish(),
   mask: string().nullish(),
+  model: string().default('inpaint'),
 })
 
 export const avatarRouter = router({
   generate: publicProcedure.input(generateInput).mutation(async ({ input }) => {
+    const model = models[input.model] || models.inpaint
+
     const output = await replicate.predictions.create({
-      version:
-        '1bfb924045802467cf8869d96b231a12e6aa994abfe37e337c63a4e49a8c6c41',
+      version: model.version,
       input: {
         prompt: input.prompt,
         image: input.avatar,
